743fc03487154d7c78ddf20262806345cb2d9583
theRaffe <rafe2004@gmail.com>
2015-11-09 08:35:43 -0600

    changes database

---
+++ db.sda.tracker/initial_data.sql
@@ -0,0 +1,34 @@
+delete from cat_environment;
+insert into cat_environment values(1, 'maab.des', 'maab dev, fix bugs and new feat.');
+insert into cat_environment values(2, 'maab.qas', 'maab release, new future feature');
+insert into cat_environment values(3, 'maab.uat', 'maab hotfix, bug on production');
+insert into cat_environment values(4, 'maab.qa2', 'maab incognito/broadsoft');
+insert into cat_environment values(5, 'maab.prd', 'maab production');
+insert into cat_environment values(6, 'izzi.des', 'izzi dev, fix bugs and new feat.');
+insert into cat_environment values(7, 'izzi.qas', 'izzi release, new future feature');
+insert into cat_environment values(8, 'izzi.uat', 'izzi hotfix, bug on production');
+insert into cat_environment values(9, 'izzi.qa2', 'izzi incognito/broadsoft');
+insert into cat_environment values(10, 'izzi.prd', 'izzi production');
+
+delete from cat_artifact
+insert into cat_artifact values(1, 'cartridge-1', 'cartridge1', 'artifact example');
+
+delete form cat_branch_git;
+insert into cat_branch_git values(1, 'des', 1, 'development of hotfixes and new feat.');
+insert into cat_branch_git values(2, 'qas', 2, 'development of new features');
+insert into cat_branch_git values(3, 'uat', 3, 'development of hotfixes for production');
+insert into cat_branch_git values(4, 'master', 5, 'production code');
+insert into cat_branch_git values(5, 'brs.izzi.develop', 6, 'development of bugfixes and new feat.');
+insert into cat_branch_git values(6, 'brs.izzi.release', 7, 'development of new features');
+insert into cat_branch_git values(7, 'brs.izzi.hotfixes', 8, 'development of hotfixes');
+insert into cat_branch_git values(8, 'brs.izzi.master', 10, 'code production new architecture');
+
+delete from cat_status_ticket;
+insert into cat_status_ticket values(1, 'requested', 'ticket requested');
+insert into cat_status_ticket values(2,'installed', 'ticket installed at environment');
+
+delete from cat_type_tech;
+insert into cat_type_tech values(1, 'java', 'releases from java');
+insert into cat_type_tech values(2, 'osb', 'releases from osb');
+insert into cat_type_tech values(3, 'db', 'releases from db');
+insert into cat_type_tech values(4, 'soa', 'releases from soa');
---
+++ db.sda.tracker/scriptTables.sql
@@ -0,0 +1,62 @@
+create table cat_environment(
+  id_environment tinyint not null,
+  code_environment varchar(20) not null unique,
+  description varchar(50) not null,
+  primary key(id_environment)
+);
+
+create table cat_artifact(
+  id_artifact tinyint not null,
+  code_artifact varchar(20) not null unique,
+  path_directory varchar(50) not null,
+  description varchar(100) not null,
+  primary key(id_artifact)
+);
+
+create table cat_branch_git(
+  id_branch tinyint not null,
+  code_branch varchar(20) not null unique,
+  id_environment_def tinyint not null,
+  description varchar(50),
+  primary key(id_branch),
+  foreign key(id_environment_def) references cat_environment(id_environment)
+);
+
+
+create table cat_status_ticket(
+  id_status tinyint not null,
+  code_status varchar(20) not null unique,
+  description varchar(50),
+  primary key(id_status)
+);
+
+create table cat_type_tech(
+  id_type_tech tinyint not null,
+  code_type_tech varchar(20) not null unique,
+  description varchar(50) not null,
+  primary key(id_type_tech)
+);
+
+create table ticket_board(
+  id_ticket varchar(20) not null,
+  id_environment tinyint not null,
+  id_status tinyint not null,
+  date_requested integer not null,
+  date_installed integer not null,
+  primary key(id_ticket),
+  foreign key(id_environment) references cat_environment(id_environment),
+  foreign key(id_status) references cat_status_ticket(id_status)
+);
+
+create table ticket_artifact(
+  id_ticket varchar(20) not null,
+  id_artifact tinyint not null,
+  id_type_tech tinyint not null,
+  creation_user varchar(20) not null,
+  creation_date integer not null,
+  modification_user varchar(20),
+  modification_date integer,
+  primary key(id_ticket, id_artifact, id_type_tech),
+  foreign key(id_artifact) references cat_artifact(id_artifact),
+  foreign key(id_type_tech) references cat_type_tech(id_type_tech)
+);
