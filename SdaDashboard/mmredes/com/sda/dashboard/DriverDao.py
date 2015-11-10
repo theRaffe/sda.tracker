ddc507e3e9e1d69bac1b9d3656c6262d4148574c
theRaffe <rafe2004@gmail.com>
2015-11-10 05:24:30 -0600

    adding dao for sqlite

--- SdaDashboard/mmredes/com/sda/dashboard/DriverDao.py
+++ SdaDashboard/mmredes/com/sda/dashboard/DriverDao.py
@@ -1,6 +1,6 @@
 __author__ = 'macbook'
 from mmredes.com.sda.dashboard.dao.SdaTrackerDao import SdaTrackerDao
-
+import sqlite3 as lite
 
 class DriverDao:
     dao_object = None
@@ -12,34 +12,42 @@ class DriverDao:
     def get_artifacts(self):
         return self.dao_object.get_artifacts()
 
+    def get_list_artifacts(self):
+        return self.dao_object.get_list_artifacts()
+
     def get_ticket_board(self, ticket):
-        return self.dao_object.get_ticket(ticket)
+        row = self.dao_object.get_ticket(ticket)
+        return dict(zip(row.keys(), row))
 
     def get_ticket_artifact(self, ticket, id_artifact, type_tech):
         return None
 
-    def manage_ticket_db(self, dict_branch):
-        for ticket in dict_branch:
-            print "search ticket"
-            ticket_row = self.get_ticket_board(ticket)
+    def process_ticket_db(self, dict_branch):
+        try:
+            for ticket in dict_branch:
+                print "search ticket"
+                dict_ticket = self.get_ticket_board(ticket)
 
-            if ticket_row:
-                ls_artifact = dict_branch[ticket]
-                print "update ticket_board.date_requested"
-                print "search ticket_artifact by id_artifact, type_tech"
+                if dict_ticket:
+                    ls_artifact = dict_branch[ticket]
+                    print "update ticket_board.date_requested"
+                    self.dao_object.update_ticket_board(dict_ticket)
+                    print "search ticket_artifact by id_artifact, type_tech"
 
-                for dict_artifact in ls_artifact:
-                    id_artifact = dict_artifact["id_artifact"]
-                    id_type_tech = dict_artifact["type_tech"]
-                    row_ticket_artifact = self.get_ticket_artifact(ticket, id_artifact, id_type_tech)
-                    if row_ticket_artifact:
-                        print "update ticket_artifact.modified_date, ticket_artifact.modifier_email"
-                    else:
-                        print "create artifact id_artifact, type_tech, creator_email, creation_date"
-            else:
-                print "create ticket_board, id_environment, id_ticket, id_status, date_requested"
-                print "create ticket_artifact id_ticket, id_artifact, creator_email, creation_date"
+                    for dict_artifact in ls_artifact:
+                        id_artifact = dict_artifact["id_artifact"]
+                        id_type_tech = dict_artifact["type_tech"]
+                        row_ticket_artifact = self.get_ticket_artifact(ticket, id_artifact, id_type_tech)
+                        if row_ticket_artifact:
+                            print "update ticket_artifact.modified_date, ticket_artifact.modifier_email"
+                        else:
+                            print "create artifact id_artifact, type_tech, creator_email, creation_date"
+                else:
+                    print "create ticket_board, id_environment, id_ticket, id_status, date_requested"
+                    print "create ticket_artifact id_ticket, id_artifact, creator_email, creation_date"
+        except lite.Error, e:
+            print "Error Database %s:" % e.args
 
 
 driver = DriverDao()
-print driver.get_artifacts()
+print driver.get_list_artifacts()
--- SdaDashboard/mmredes/com/sda/dashboard/dao/SdaTrackerDao.py
+++ SdaDashboard/mmredes/com/sda/dashboard/dao/SdaTrackerDao.py
@@ -1,4 +1,5 @@
 import sqlite3 as lite
+import time
 import sys
 
 __author__ = 'macbook'
@@ -24,6 +25,16 @@ class SdaTrackerDao:
 
         return dict_artifact
 
+    def get_list_artifacts(self):
+        self.conn.row_factory = lite.Row
+        cur = self.conn.cursor()
+        cur.execute("select * from cat_artifact")
+        rows = cur.fetchall()
+        result = []
+        for row in rows:
+            result.append(dict(zip(row.keys(), row)))
+        return result
+
     def get_ticket(self, id_ticket):
         return self.get_ticket(id_ticket, 0)
 
@@ -33,3 +44,12 @@ class SdaTrackerDao:
         cur.execute("select * from ticket_board where id_ticket = :id_ticket and id_status = :id_status",
                     {"id_ticket": id_ticket, "id_status": id_status})
         return cur.fetchone()
+
+    def update_ticket_board(self, dict_ticket_board):
+        cur = self.conn.cursor()
+        date_requested = time.time()
+        cur.execute(
+            "update ticket_board set date_requested = :date_requested, user_request = :user_request where id_ticket = :id_ticket and id_environment = :id_environment",
+            {"date_requested": date_requested, "user_request": dict_ticket_board["user_request"],
+             "id_ticket": dict_ticket_board["id_ticket"], "id_environment": dict_ticket_board["id_environment"]})
+        self.conn.commit()
