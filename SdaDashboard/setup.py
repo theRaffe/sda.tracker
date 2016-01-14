abfaa9ab9b5f437455c1aaf179e634deea640a1d
rafael.briones <rafael.briones.ext@multimedios.net>
2016-01-14 17:12:20 -0600

    add gitignore add SdaDashboard/setup.py

---
+++ SdaDashboard/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+        name='SdaDashboard',
+        version='',
+        packages=['mmredes', 'mmredes.com', 'mmredes.com.sda', 'mmredes.com.sda.emailing', 'mmredes.com.sda.dashboard',
+                  'mmredes.com.sda.dashboard.dao', 'mmredes.com.sda.dashboard.test',
+                  'mmredes.com.sda.dashboard.management', 'mmredes.com.sda.dashboard.repository'],
+        url='',
+        license='',
+        author='Rafael',
+        author_email='',
+        description=''
+)
