c670e7d3e5cdd206f3e06dfa7073dbbe63bcf3f7
rafael.briones <rafael.briones.ext@multimedios.net>
2016-05-26 11:46:17 -0500

    fixing DriverEmailTracker.py in order to avoid this error:   File "C:\Python27\lib\site-packages\win32com\client\__init__.py", line 170, in __getattr__     raise AttributeError(a) AttributeError: olFolderInbox

--- SdaDashboard/mmredes/com/sda/utils/DriverEmailTracker.py
+++ SdaDashboard/mmredes/com/sda/utils/DriverEmailTracker.py
@@ -13,7 +13,7 @@ class DriverEmailTracker:
     def __init__(self, filename="LOG1.txt", host_port=""):
         # mailbox="Archive Rafael Briones", folderindex=1):
         self.f = FileWriter(filename)
-        self.outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+        self.outlook = win32com.client.gencache.EnsureDispatch("Outlook.Application").GetNamespace("MAPI")
         inbox = self.outlook.GetDefaultFolder(win32com.client.constants.olFolderInbox)
         self.inbox = inbox
         self.host_port = host_port
@@ -22,6 +22,7 @@ class DriverEmailTracker:
         subject_defect = "QC_MAAB.Sistemas_TVI - Defect"
         search_subject_defect = "'%s%%'" % subject_defect
         subject_update_env = unicode(u'Actualización Ambientes Testing')
+        subject_update_env2 = unicode(u'Actualizacion Ambientes Testing')
         search_subject_update_env = "'%Ambientes Testing'"
         restrict_criteria = "@SQL=\"urn:schemas:httpmail:read\" = 0 And (\"urn:schemas:mailheader:subject\"  like %s Or \"urn:schemas:mailheader:subject\" like %s)" % (
             search_subject_defect, search_subject_update_env)
@@ -40,7 +41,7 @@ class DriverEmailTracker:
                 process_defect_email(message.HTMLBody, self.host_port)
 
             unicode_subject = unicode(message.Subject)
-            if subject_update_env == unicode_subject:
+            if subject_update_env == unicode_subject or subject_update_env2 == unicode_subject:
                 print 'message.SenderEmailAddress = %s' % message.SenderName
                 process_updating_environment(message.HTMLBody, self.host_port, message.SenderName)
             message.UnRead = False
